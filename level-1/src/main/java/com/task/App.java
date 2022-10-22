9d35bd005be6a9b34f5ea413d72060cce478ff50
Dragosh Sergei <dragoshs.j@yahoo.com>
2022-10-22 22:00:24 +0300

    Solved the task 1480
    Changed .gitignore file

---
+++ level-1/src/main/java/com/task/App.java
@@ -0,0 +1,16 @@
+package com.task;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int[] nums = new int[]{1,2,3,4};
+        System.out.println(Arrays.toString(new com.task.t1480.Solution().runningSum(nums)));
+    }
+}
