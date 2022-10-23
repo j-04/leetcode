72ee5f491ab8f1e6860119f1ba7ca473c4ac4c5f
Dragosh Sergei <dragoshs.j@yahoo.com>
2022-10-23 20:32:24 +0300

    Solved the task 205

---
+++ level-1/src/main/java/com/task/t205/Solution205.java
@@ -0,0 +1,27 @@
+package com.task.t205;
+
+import java.util.HashMap;
+
+public class Solution205 {
+    public boolean isIsomorphic(String s, String t) {
+        var maps = new HashMap<Character, Character>();
+        var mapt = new HashMap<Character, Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char symbolS = s.charAt(i);
+            char symbolT = t.charAt(i);
+            if (maps.containsKey(symbolS)) {
+                if (maps.get(symbolS) != symbolT) {
+                    return false;
+                }
+            } else if (mapt.containsKey(symbolT)) {
+                if (mapt.get(symbolT) != symbolS) {
+                    return false;
+                }
+            }
+            maps.put(symbolS, symbolT);
+            mapt.put(symbolT, symbolS);
+        }
+        return true;
+    }
+}
