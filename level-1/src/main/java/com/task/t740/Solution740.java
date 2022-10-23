1b6341363e0e5439c7ef32ffa143dc34b861749f
Dragosh Sergei <dragoshs.j@yahoo.com>
2022-10-23 19:29:28 +0300

    Added other solution for the task 740

--- level-1/src/main/java/com/task/t740/Solution740.java
+++ level-1/src/main/java/com/task/t740/Solution740.java
@@ -2,16 +2,31 @@ package com.task.t740;
 
 public class Solution740 {
     public int pivotIndex(int[] nums) {
+        // int left = 0;
+        // for (int pivot = 0; pivot < nums.length; pivot++) {
+        //     int right = 0;
+        //     for (int j = pivot + 1; j < nums.length; j++) {
+        //         right += nums[j];
+        //     }
+        //     if (left == right) {
+        //         return pivot;
+        //     }
+        //     left += nums[pivot];
+        // }
+        // return -1;
         int left = 0;
-        for (int pivot = 0; pivot < nums.length; pivot++) {
-            int right = 0;
-            for (int j = pivot + 1; j < nums.length; j++) {
-                right += nums[j];
-            }
+        int right = 0;
+        for (int num: nums) {
+            right += num;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            right -= num;
             if (left == right) {
-                return pivot;
+                return i;
             }
-            left += nums[pivot];
+            left += num;
         }
         return -1;
     }
