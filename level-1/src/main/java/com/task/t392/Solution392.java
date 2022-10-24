3cd7a27441b75fbae8f4c2079eb786a19baabbe7
Dragosh Sergei <dragoshs.j@yahoo.com>
2022-10-24 10:46:04 +0300

    Solved the task 392

---
+++ level-1/src/main/java/com/task/t392/Solution392.java
@@ -0,0 +1,15 @@
+package com.task.t392;
+
+public class Solution392 {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
