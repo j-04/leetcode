6d94d79b8c8b621e3dd02f8509e616fc8f3ad6cf
Dragosh Sergey <dragoshs.j@yahoo.com>
2023-05-20 21:21:56 +0300

    Solve Task 15 3Sum

---
+++ src/main/java/com/task/t15/Solution.java
@@ -0,0 +1,38 @@
+package com.task.t15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 3Sum
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        final List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i-1]) {
+                int left = i + 1;
+                int right = nums.length - 1;
+                int sum = 0 - nums[i];
+                while (left < right) {
+                    if (nums[left] + nums[right] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left+1])  {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right-1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] < sum) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
